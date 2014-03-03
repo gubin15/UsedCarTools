1f71ec3d066a86e4e7ed655d8f4cd52cbe8d851a
AppleGu <15635982@qq.com>
2014-03-03 22:19:27 +0800

    Initial Commit

---
+++ UsedCarTools/AJQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AJQAppDelegate.h
+//  UsedCarTools
+//
+//  Created by 顾斌 on 14-3-3.
+//  Copyright (c) 2014年 顾斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
